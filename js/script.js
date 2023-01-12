876772559d6486d49a107304ecd6754082c088e2
Louis B <47404526+ElBarbs@users.noreply.github.com>
2023-01-12 14:04:55 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/**
+Getting Set Up.
+Louis Barbier.
+
+Learning about GIT.
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(500, 500);
+  background("green");
+}
+
+/**
+Description of draw()
+*/
+function draw() {}
